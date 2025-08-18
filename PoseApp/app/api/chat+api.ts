53f64de552c57f5afdc4db79d0974e191ec95a1c
Bryant Zhou <btyzt0@gmail.com>
2025-08-18 08:38:48 -0400

    modify the agents

--- PoseApp/app/api/chat+api.ts
+++ PoseApp/app/api/chat+api.ts
@@ -1,12 +1,16 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { streamText, UIMessage, convertToModelMessages, stepCountIs, smoothStream } from 'ai';
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
     model: openai('gpt-4o'),
+    system: "You are a orthopedic doctor who help patients correct their body postures post surgery. You will the results from the pose estimation models, \
+             and instruct patients how they should change postures or tell them they are in good shape.",
+    stopWhen: stepCountIs(5),
     messages: convertToModelMessages(messages),
+    experimental_transform: smoothStream(),
   });
 
   return result.toUIMessageStreamResponse({
